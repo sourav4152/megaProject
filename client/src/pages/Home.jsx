be5bce07b5e9da3aec7f79322e4f6da33161ace8
sourav4152 <guptasourav595@gmail.com>
2025-07-27 19:49:06 +0530

    Review section pending

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,6 +7,12 @@ import Banner from '../assets/Image/banner.mp4'
 import HighlightText from '../components/core/HomePage/HighlightText'
 import CTAButton from '../components/core/HomePage/Button'
 import CodeBlocks from '../components/core/HomePage/CodeBlocks'
+import ExploreMore from '../components/core/HomePage/ExploreMore'
+import TimeLineSection from '../components/core/HomePage/TimeLineSection'
+import LearningLanguageSection from '../components/core/HomePage/LearningLanguageSection'
+import InstructorSection from '../components/core/HomePage/InstructorSection'
+import ReviewSlider from '../components/core/HomePage/ReviewSlider'
+import Footer from '../components/common/Footer'
 
 const Home = () => {
     return (
@@ -94,16 +100,101 @@ const Home = () => {
                 </div>
 
                 {/* code Section 2 */}
+                <div>
+                    <CodeBlocks
+                        position={"lg:flex-row-reverse"}
+                        heading={
+                            <div className="w-[100%] text-4xl font-semibold lg:w-[50%]">
+                                Start your journey and
+                                <HighlightText text={"begin coding in seconds"} />
+                            </div>
+                        }
+                        subheading={
+                            "Go ahead, give it a try. Our hands-on learning environment means you'll be writing real code from your very first lesson."
+                        }
+                        ctaBtn1={{
+                            btnText: "Continue Lesson",
+                            link: "/signup",
+                            active: true,
+                        }}
+                        ctaBtn2={{
+                            btnText: "Learn More",
+                            link: "/signup",
+                            active: false,
+                        }}
+                        codeColor={"text-white"}
+                        codeBlock={`import React from "react";\n import CTAButton from "./Button";\nimport TypeAnimation from "react-type";\nimport { FaArrowRight } from "react-icons/fa";\n\nconst Home = () => {\nreturn (\n<div>Home</div>\n)\n}\nexport default Home;`}
+                        backgroundGradient={<div className="codeblock2 absolute"></div>}
+                    />
+                </div>
 
+                {/* Explore Section */}
+                <ExploreMore />
 
             </div>
 
 
             {/* Section 2 */}
+            <div className='bg-pure-greys-5 text-richblack-700'>
+
+                <div className="homepage_bg h-[320px]">
+                    {/* Explore Full Category Section */}
+                    <div className="mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8">
+                        <div className="lg:h-[150px]"></div>
+                        <div className="flex flex-row gap-7 text-white lg:mt-8">
+                            <CTAButton active={true} linkTo={"/signup"}>
+                                <div className="flex items-center gap-2">
+                                    Explore Full Catalog
+                                    <FaArrowRight />
+                                </div>
+                            </CTAButton>
+                            <CTAButton active={false} linkTo={"/login"}>
+                                Learn More
+                            </CTAButton>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 ">
+                    {/* Job that is in Demand - Section 1 */}
+                    <div className="mb-10 mt-[-100px] flex flex-col justify-between gap-7 lg:mt-20 lg:flex-row lg:gap-0">
+                        <div className="text-4xl font-semibold lg:w-[45%] ">
+                            Get the skills you need for a{" "}
+                            <HighlightText text={"job that is in demand."} />
+                        </div>
+                        <div className="flex flex-col items-start gap-10 lg:w-[40%]">
+                            <div className="text-[16px]">
+                                The modern educational landscape dictates its own terms. Today, being a competitive specialist requires more than just professional skills.
+                            </div>
+                            <CTAButton active={true} linkTo={"/signup"}>
+                                <div className="">Learn More</div>
+                            </CTAButton>
+                        </div>
+                    </div>
+
+                    <TimeLineSection />
+
+                    <LearningLanguageSection />
+
+                </div>
+
+            </div>
 
             {/* Section 3 */}
+            <div className="relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white">
+                {/* Become a instructor section */}
+                <InstructorSection />
+
+                {/* Review from Other Learner */}
+                <h1 className="text-center text-4xl font-semibold mt-8">
+                    Reviews From Other Learner
+                </h1>
+
+                <ReviewSlider />
+            </div>
 
             {/* Footer */}
+             <Footer />           
 
         </div>
     )
