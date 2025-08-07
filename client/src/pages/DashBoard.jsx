51c72043bf2e258d5dfc22e35b44dbf485fb32ca
sourav4152 <guptasourav595@gmail.com>
2025-08-07 18:08:57 +0530

    overflow-y:hidden add to solve add course overflow **

--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -28,7 +28,7 @@ const DashBoard = () => {
     }
 
     return (
-        <div className="relative flex min-h-[calc(100vh-3.5rem)]">
+        <div className="relative flex min-h-[calc(100vh-3.5rem)] overflow-y-hidden">
             <Sidebar />
             <div className="h-[calc(100vh-3.5rem)] flex-1 overflow-auto">
                 <div className="mx-auto w-11/12 max-w-[1000px] py-10">
