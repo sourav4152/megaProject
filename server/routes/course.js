1aae8090557eb5d92611e5e2a9c9c57097ed3acd
sourav4152 <guptasourav595@gmail.com>
2025-08-08 21:19:03 +0530

    added pending routes

--- server/routes/course.js
+++ server/routes/course.js
@@ -3,7 +3,7 @@ const route =express.Router();
 
 const {createCategory, categoryPageDetails} =require("../controllers/category");
 
-const {createCourse, getCourseDetails, getEnrolledCourses, getInstructorCourses,deleteCourse} =require("../controllers/course");
+const {createCourse, getCourseDetails, getEnrolledCourses, getInstructorCourses,deleteCourse, editCourse} =require("../controllers/course");
 
 
 const {showAllCategories} =require("../controllers/category");
@@ -12,7 +12,7 @@ const {showAllCategories} =require("../controllers/category");
 const {createSection, updateSection, deleteSection} =require("../controllers/section")
 
 
-const {createSubSection} = require("../controllers/subSection")
+const {createSubSection, deleteSubSection} = require("../controllers/subSection")
 
 const {createRating, getAverageRating, getAllRating} =require("../controllers/ratingAndReview")
 
@@ -29,14 +29,17 @@ route.get("/showAllCategories",showAllCategories);
 
 route.post("/createCourse", auth, isInstructor, createCourse);
 route.post("/getYourCourses", auth, getEnrolledCourses);
+route.put("/updateCourse",auth,isInstructor,editCourse)
 route.put("/deleteCourse",auth, isInstructor,deleteCourse)
 
 
 route.post("/addSection", auth, isInstructor, createSection);
 route.post("/updateSection", auth, isInstructor, updateSection);
-route.post("/deleteSection", auth, isInstructor, deleteSection);
+route.delete("/deleteSection", auth, isInstructor, deleteSection);
 
 route.post("/addSubSection", auth,isInstructor, createSubSection);
+route.post('/updateSubSection',auth,isInstructor,updateSection);
+route.delete('/deleteSubSection',auth,isInstructor,deleteSubSection)
 
 
 route.post("/createRating", auth ,isStudent, createRating);
