255bc2506683fe2e3928e7b3f495aed19c7416f8
sourav4152 <guptasourav595@gmail.com>
2025-08-13 20:52:00 +0530

    category page controller modified

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -121,43 +121,70 @@ exports.showAllCategories = async (req, res) => {
 
 //category Page details 
 exports.categoryPageDetails = async (req, res) => {
-    try {
+  try {
+    const { categoryId } = req.body;
 
-        const { categoryId } = req.body;
+    const selectedCategory = await Category.findById(categoryId)
+      .populate({
+        path: "courses",
+        match: { status: "Published" },
+        populate: "ratingAndReviews",
+      })
+      .exec();
 
-        const selectedCategory = await Category.findById(categoryId)
-            .populate("course")
-            .exec();
-
-        if (!selectedCategory) {
-            return res.status(404).json({
-                success: false,
-                message: "No course found"
-            })
-        }
-
-        const differentCategories = await Category.find({ _id: { $ne: categoryId } })
-                                    .populate("course")
-                                    .exec();
-        // find most popular courses
-        // top selling courses
-
-        return res.status(200).json({
-            success:true,
-            message:"Courses sent successfully",
-            data:{
-                selectedCategory,
-                differentCategories,
-                //sent most popular and most selling too
-            }
-        })
-
-
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({
-            success: false,
-            message: "Something went wrong while fetching Categories courses"
-        });
+    if (!selectedCategory) {
+      console.log("Category not found.");
+      return res
+        .status(404)
+        .json({ success: false, message: "Category not found" });
     }
-}
+
+    if (selectedCategory.courses.length === 0) {
+      console.log("No courses found for the selected category.");
+      return res.status(200).json({
+        success: true,
+        message: "No courses found for the selected category.",
+      });
+    }
+
+    const categoriesExceptSelected = await Category.find({
+      _id: { $ne: categoryId },
+    });
+    let differentCategory = await Category.findOne(
+      categoriesExceptSelected[getRandomInt(categoriesExceptSelected.length)]
+        ._id
+    )
+      .populate({
+        path: "courses",
+        match: { status: "Published" },
+      })
+      .exec();
+    console.log();
+
+    const allCategories = await Category.find()
+      .populate({
+        path: "courses",
+        match: { status: "Published" },
+      })
+      .exec();
+    const allCourses = allCategories.flatMap((category) => category.courses);
+    const mostSellingCourses = allCourses
+      .sort((a, b) => b.sold - a.sold)
+      .slice(0, 10);
+
+    res.status(200).json({
+      success: true,
+      data: {
+        selectedCategory,
+        differentCategory,
+        mostSellingCourses,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
